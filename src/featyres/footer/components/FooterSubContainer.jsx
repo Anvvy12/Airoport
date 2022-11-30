202d7b49f3a3719147c3770105daa570d56a89cd
Bogdan <youbody7@gmail.com>
2022-11-30 16:25:57 +0200

    s

--- src/featyres/footer/components/FooterSubContainer.jsx
+++ src/featyres/footer/components/FooterSubContainer.jsx
@@ -5,10 +5,11 @@ const FooterSubContainer = () => {
   return (
     <div className="container">
       <div className="sub-footer">
-        <div className="footer-row">
-          <div className="top-footer-row"></div>
-          <div className="bottom-footer-row"></div>
+        <div className="info">
+          <div>sssssss</div>
+          <div>aaaaaaaaaa</div>
         </div>
+        <div className="made-by">ffffffffff</div>
       </div>
     </div>
   );
