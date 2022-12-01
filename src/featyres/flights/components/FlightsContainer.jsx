774e2e9b83037a867821542d2657a4fa3bda632b
Bogdan <youbody7@gmail.com>
2022-12-01 15:59:29 +0200

    s

--- src/featyres/flights/components/FlightsContainer.jsx
+++ src/featyres/flights/components/FlightsContainer.jsx
@@ -7,8 +7,8 @@ const FlightsContainer = () => {
     <div className="flights-container">
       <div className="flights-nav-site">
         <ul className="flights-sortList">
-          <li className="flights-sortList__item actual">ВИЛІТ</li>
-          <li className="flights-sortList__item">ПРИЛІТ</li>
+          <li className="flights-sortList__item actual left-side">ВИЛІТ</li>
+          <li className="flights-sortList__item secondary right-side">ПРИЛІТ</li>
         </ul>
       </div>
       <FlightsTablet />
