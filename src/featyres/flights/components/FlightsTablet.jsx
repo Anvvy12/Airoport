32600a170a7eb250b38b5d19bada470187183ffa
Bogdan <youbody7@gmail.com>
2022-12-01 17:55:46 +0200

    s

--- src/featyres/flights/components/FlightsTablet.jsx
+++ src/featyres/flights/components/FlightsTablet.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import FlightsTabletRow from './FlightsTabletRow';
-import '../styles/flights.scss';
+import '../styles/tablet-container.scss';
 
 const FlightsTablet = () => {
-  return <FlightsTabletRow />;
+  return (
+    <div className="result-container">
+      <div className="tablet-container">
+        <div className="data-container">Data Component</div>
+        <div className="result-tablet">
+          <table className="table">
+            <thead>sssssssss</thead>
+            <FlightsTabletRow />
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FlightsTablet;
