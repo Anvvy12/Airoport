7918a5da50afc18c444840f3354c0b19fdc37c76
Bogdan <youbody7@gmail.com>
2022-12-02 12:05:24 +0200

    s

---
+++ src/featyres/flights/components/DataContainer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../styles/data-container.scss';
+
+const DataContainer = () => {
+  return (
+    <div className="calendar-date-wrapper">
+      <div className="calendar-container ">
+        <button className="calendar-button"></button>
+      </div>
+      <div className="dates-container  ">
+        <div className="date esterday">
+          <div className="data-num">01/12</div>
+          <div className="data-title">вчора</div>
+        </div>
+        <div className="date today">
+          <div className="data-num">02/12</div>
+          <div className="data-title">сьогодні</div>
+        </div>
+        <div className="date tommorow">
+          <div className="data-num">03/12</div>
+          <div className="data-title">завтра</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DataContainer;
--- src/featyres/flights/components/FlightsTablet.jsx
+++ src/featyres/flights/components/FlightsTablet.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import FlightsTabletRow from './FlightsTabletRow';
-import '../styles/tablet-container.scss';
+import DataContainer from './DataContainer';
+import '../styles/result-container.scss';
 
 const FlightsTablet = () => {
   return (
     <div className="result-container">
       <div className="tablet-container">
-        <div className="data-container">Data Component</div>
+        <DataContainer />
+
         <div className="result-tablet">
           <table className="table">
-            <thead>sssssssss</thead>
+            <thead className="table-head">
+              <tr>
+                <td>sssssssss</td>
+              </tr>
+            </thead>
             <FlightsTabletRow />
           </table>
         </div>
