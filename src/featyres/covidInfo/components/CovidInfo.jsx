ea5e676d9633859ef046b8ecd57b3e0eeaa5d6cb
Bogdan <youbody7@gmail.com>
2022-12-02 16:20:30 +0200

    s

--- src/featyres/covidInfo/components/CovidInfo.jsx
+++ src/featyres/covidInfo/components/CovidInfo.jsx
@@ -5,7 +5,13 @@ const CovidInfo = () => {
   return (
     <div className="covid-container">
       <div className="covid-info">
-        <div className="covid-title-"></div>
+        <a className="covid-info__link">
+          <span className="covid-info__link-text">
+            Тестування на COVID-19 у аеропорту «Київ»
+            <br />
+            <small className="covid-info__link-text-small">Для всіх охочих 24/7</small>
+          </span>
+        </a>
       </div>
     </div>
   );
