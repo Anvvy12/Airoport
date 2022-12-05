573863abcddbf7ccece84bc735a1e854e9b3ea90
Bogdan <youbody7@gmail.com>
2022-12-05 14:51:45 +0200

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import Header from './featyres/header/components/Header';
 import Footer from './featyres/footer/components/Footer';
 import Main from './featyres/main/components/Main';
+import store from './store';
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <Header />
       <div className="wrapper-container">
         <Main />
         <Footer />
       </div>
-    </>
+    </Provider>
   );
 };
 
