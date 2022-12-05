83cc4c9f10ceda618f8692767e49c2d2921cfd5c
Bogdan <youbody7@gmail.com>
2022-12-05 14:36:53 +0200

    s

--- src/store.js
+++ src/store.js
@@ -1,3 +1,13 @@
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import flightsReducer from './featyres/table/table.reducer';
 import thunk from 'redux-thunk';
+
+const reducers = combineReducers({ flights: flightsReducer });
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+const store = createStore(reducers, enhancer);
+
+export default store;
