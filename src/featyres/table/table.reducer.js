af699b5144447287c3840f63f6f6322b3dcdf2c6
Bogdan <youbody7@gmail.com>
2022-12-07 13:50:37 +0200

    s

--- src/featyres/table/table.reducer.js
+++ src/featyres/table/table.reducer.js
@@ -3,8 +3,6 @@ import { FILL_TABLE_START_FLIGHTS } from './table.actions';
 const initialState = { departure: [], arrival: [] };
 
 const flightsReducer = (state = initialState, action) => {
-  // console.log(action.payload.departure);
-  // console.log(action.payload.arival);
   switch (action.type) {
     case FILL_TABLE_START_FLIGHTS:
       return {
