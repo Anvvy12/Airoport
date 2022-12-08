311c385b9b6949e10c5c68636c33af88a7070bf8
Bogdan <youbody7@gmail.com>
2022-12-08 16:01:56 +0200

    s

--- src/featyres/main/components/Main.jsx
+++ src/featyres/main/components/Main.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import Search from '../../search/components/Search';
 import ResultContainerDepart from '../../flights/components/ResultContainerDepart';
 import ResultContainerArrival from '../../flights/components/ResultContainerArrival';
 import CovidInfo from '../../covidInfo/components/CovidInfo';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import * as flightsActions from '../main.actions';
-import { connect } from 'react-redux';
+
 import '../styles/main.scss';
 
 const Main = ({ getFlights }) => {
@@ -36,11 +37,5 @@ const Main = ({ getFlights }) => {
 const mapDispatch = {
   getFlights: flightsActions.fetchStartFlights,
 };
-
-// const mapState = state => {
-//   return {
-//     arrival: selectors.arivalSelector(state),
-//   };
-// };
 
 export default connect(null, mapDispatch)(Main);
