43aba328d9e0d4b0594c7a8cfcfb48a9234b35df
Bogdan <youbody7@gmail.com>
2023-01-10 16:06:20 +0200

    s

--- src/featyres/table/components/TableHead.jsx
+++ src/featyres/table/components/TableHead.jsx
@@ -3,26 +3,26 @@ import React from 'react';
 const TableHeadArrival = ({ text }) => {
   return (
     <thead className="table-head">
-      <tr className="table-head-row term">
-        <th className="table-down" scope="col">
+      <tr className="table-head-row ">
+        <th className="table-down term" scope="col">
           Термінал
         </th>
-        <th className="table-down" scope="col">
+        <th className="table-down timetable start" scope="col">
           Розклад
         </th>
-        <td className="table-down" scope="col">
+        <td className="table-down ariv-dep start" scope="col">
           {text}
         </td>
-        <th className="table-down" scope="col">
+        <th className="table-down status start" scope="col">
           Статус
         </th>
-        <th className="table-down" scope="col">
+        <th className="table-down company start" scope="col">
           Авіакомпанія
         </th>
-        <th className="table-down" scope="col">
+        <th className="table-down rais start" scope="col">
           Рейс
         </th>
-        <td className="table-down" scope="col"></td>
+        <td className="table-down start" scope="col"></td>
       </tr>
     </thead>
   );
--- src/featyres/table/components/departure/TableRowDeparture.jsx
+++ src/featyres/table/components/departure/TableRowDeparture.jsx
@@ -4,16 +4,16 @@ const FlightstableRow = ({ term, logo, cityTo, num, companyName, timeCalc, timeF
   const timeDepartureFakt = timeFact ? timeFact.substring(12, 16) : '00:00';
   const timeDepartureCalc = timeCalc ? timeCalc.substring(12, 16) : '00:00';
 
-  const classTerm = term == 'A' ? 'table-down__termA term' : 'table-down__termB term';
+  const classTerm = term == 'A' ? 'table-down__termA ' : 'table-down__termB ';
   return (
     <tr className="table-row flights">
-      <th className="table-down term">
+      <th className="table-down">
         <span className={classTerm}>{term}</span>
       </th>
-      <th className="table-down">{timeDepartureCalc}</th>
-      <td className="table-down city">{cityTo}</td>
-      <th className="table-down">Вилетів o {timeDepartureFakt}</th>
-      <th className="table-down">
+      <th className="table-down start">{timeDepartureCalc}</th>
+      <td className="table-down city start">{cityTo}</td>
+      <th className="table-down start">Вилетів o {timeDepartureFakt}</th>
+      <th className="table-down start">
         <ul className="company-list">
           <li className="company-list__item">
             <div data-v-7746f986="" className="flights-logo-container">
@@ -24,8 +24,8 @@ const FlightstableRow = ({ term, logo, cityTo, num, companyName, timeCalc, timeF
           </li>
         </ul>
       </th>
-      <th className="table-down">{num}</th>
-      <td className="table-down ditals">
+      <th className="table-down start">{num}</th>
+      <td className="table-down ditals start">
         <a className="ditals__text">Деталі рейсу</a>
       </td>
     </tr>
